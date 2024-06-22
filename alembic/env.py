430258cce4fadf414d759cfaf5db310f2a130ea9
ericsonnascimento <ericson@grupocyber.com.br>
2024-06-22 09:39:20 -0300

    bug and upgdate - ajustes de bugs nos arquivos models das três tabelas, ajuste no arquivo comandos importantes, criação das funções dentro do Makefiles: make create-migrations d='init_db' e make run-migrations, e por fim criação do banco de dados no postgresql dentro do container

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,10 @@
+import asyncio
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config
+
+from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.engine import Connection
 from sqlalchemy import pool
+
 from alembic import context
 from workout_api.contrib.models import BaseModel
 #import de todo o DB da pasta repository onde constam apenas os imports (forma de manipular os dados de um local neltro)
@@ -44,20 +48,23 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
-def run_migrations_online() -> None:
-    connectable = engine_from_config(
+def do_run_migrations(connection: Connection) ->None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+async def run_async_migrations() -> None:
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-
-        with context.begin_transaction():
-            context.run_migrations()
+def run_migrations_online() -> None:
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
